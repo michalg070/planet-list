a55b64c57bd6631e36ce926642edf866fee2a63e
mgrzegorczyk <michal.grzegorczyk@reffine.com>
2024-06-02 13:30:13 +0200

    feat: create and style UiPagination component

---
+++ src/components/ui/UiPagination/UiPagination.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { VuePaginate } from '@svifty7/vue-paginate'
+</script>
+
+<template>
+  <VuePaginate class="ui-pagination" />
+</template>
+
+<style lang="scss" scoped>
+@import './UiPagination.scss';
+</style>
