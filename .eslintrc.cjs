468cc6e12b102ad3b848fb2acdb81a2a8e1b4f0c
mgrzegorczyk <michal.grzegorczyk@reffine.com>
2024-06-02 13:48:00 +0200

    fix: remove eslint-plugin-vitest, fix linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:vitest/recommended'
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   rules: {
     'vue/multi-word-component-names': [
@@ -19,5 +18,16 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      files: ['**/src/**/*.spec.{j,t}s?(x)'],
+      globals: {
+        describe: false,
+        it: false,
+        expect: false,
+        afterAll: false
+      }
+    }
+  ]
 }
